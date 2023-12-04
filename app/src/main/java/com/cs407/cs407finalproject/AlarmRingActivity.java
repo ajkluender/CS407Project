35c8a93e647ef3c974997438d721338a6c268664
Alexander Kluender <kluender99@gmail.com>
2023-12-04 09:28:45 -0600

    THIS IS WHAT ORION DID I JUST MOved IT FROM HIT GITHUB TO THE GROUP GIT HUB. AGAIN I DID NOT DO THIS ORION DID!!!

---
+++ app/src/main/java/com/cs407/cs407finalproject/AlarmRingActivity.java
@@ -0,0 +1,33 @@
+package com.cs407.cs407finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AlarmRingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_ring);
+
+        FragmentContainerView fragmentContainerView = findViewById(R.id.fragmentContainerView);
+        TextView greeting = findViewById(R.id.greeting);
+        Button buttonSnooze = findViewById(R.id.buttonSnooze);
+        Button buttonSolve = findViewById(R.id.buttonSolve);
+        buttonSolve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentContainerView.setVisibility(View.VISIBLE);
+                greeting.setVisibility(View.GONE);
+                buttonSnooze.setVisibility(View.GONE);
+                buttonSolve.setVisibility(View.GONE);
+            }
+        });
+    }
+}
