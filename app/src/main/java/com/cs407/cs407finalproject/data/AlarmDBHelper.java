8c449e3a0751ba28ecc75cdb00030f8ae747c75e
Alexander Kluender <kluender99@gmail.com>
2023-12-04 12:18:06 -0600

    Finished the db not sure if it works yet

--- app/src/main/java/com/cs407/cs407finalproject/data/AlarmDBHelper.java
+++ app/src/main/java/com/cs407/cs407finalproject/data/AlarmDBHelper.java
@@ -1,10 +1,15 @@
 package com.cs407.cs407finalproject.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //https://developer.android.com/training/data-storage/sqlite
 public class AlarmDBHelper extends SQLiteOpenHelper {
@@ -27,6 +32,7 @@ public class AlarmDBHelper extends SQLiteOpenHelper {
     private static final String COLUMN_SUNDAY = "sunday";
     //END DAYS
     private static final String COLUMN_CHALLENGE_TYPE = "challengeType";
+    private static final String COLUMN_TITLE = "title";
 
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + TABLE_NAME + " (" +
@@ -42,6 +48,7 @@ public class AlarmDBHelper extends SQLiteOpenHelper {
                     COLUMN_FRIDAY + " BOOLEAN," +
                     COLUMN_SATURDAY + " BOOLEAN," +
                     COLUMN_SUNDAY + " BOOLEAN," +
+                    COLUMN_TITLE + " TEXT," +
                     COLUMN_CHALLENGE_TYPE + " TEXT)";
 
 
@@ -67,5 +74,103 @@ public class AlarmDBHelper extends SQLiteOpenHelper {
     }
 
     // Add methods to insert, update, delete, and retrieve alarms
-    
+
+    public long insertAlarm(Alarm alarm) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        // Don't put id, as it's autoincrement
+        values.put(COLUMN_HOUR, alarm.getHour());
+        values.put(COLUMN_MINUTE, alarm.getMinute());
+        values.put(COLUMN_IS_RECURRING, alarm.isRecurring());
+        values.put(COLUMN_IS_ON, alarm.isOn());
+        values.put(COLUMN_MONDAY, alarm.isMonday());
+
+        values.put(COLUMN_TUESDAY, alarm.isTuesday());
+        values.put(COLUMN_WEDNESDAY, alarm.isWednesday());
+        values.put(COLUMN_THURSDAY, alarm.isThursday());
+        values.put(COLUMN_FRIDAY, alarm.isFriday());
+        values.put(COLUMN_SATURDAY, alarm.isSaturday());
+
+        values.put(COLUMN_SUNDAY, alarm.isSunday());
+        values.put(COLUMN_TITLE, alarm.getTitle());
+        values.put(COLUMN_CHALLENGE_TYPE, alarm.getChallengeType());
+
+        long id = db.insert(TABLE_NAME, null, values);
+        db.close();
+        // returns -1 if there was an error
+        return id;
+    }
+    public int updateAlarm(Alarm alarm) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_HOUR, alarm.getHour());
+        values.put(COLUMN_MINUTE, alarm.getMinute());
+        values.put(COLUMN_IS_RECURRING, alarm.isRecurring());
+        values.put(COLUMN_IS_ON, alarm.isOn());
+        values.put(COLUMN_MONDAY, alarm.isMonday());
+
+        values.put(COLUMN_TUESDAY, alarm.isTuesday());
+        values.put(COLUMN_WEDNESDAY, alarm.isWednesday());
+        values.put(COLUMN_THURSDAY, alarm.isThursday());
+        values.put(COLUMN_FRIDAY, alarm.isFriday());
+        values.put(COLUMN_SATURDAY, alarm.isSaturday());
+
+        values.put(COLUMN_SUNDAY, alarm.isSunday());
+
+        values.put(COLUMN_TITLE, alarm.getTitle());
+        values.put(COLUMN_CHALLENGE_TYPE, alarm.getChallengeType());
+
+        int count = db.update(TABLE_NAME, values, COLUMN_ID + " = ?",
+                new String[] { String.valueOf(alarm.getAlarmId()) });
+        db.close();
+
+        // returns method is the number of rows affected in the database.
+        return count;
+    }
+
+    public void deleteAlarm(int alarmId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_NAME, COLUMN_ID + " = ?",
+                new String[] { String.valueOf(alarmId) });
+        db.close();
+    }
+
+    public List<Alarm> getAllAlarms() {
+        List<Alarm> alarms = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME, null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                int alarmId = cursor.getInt(cursor.getColumnIndex(COLUMN_ID));
+                int hour = cursor.getInt(cursor.getColumnIndex(COLUMN_HOUR));
+                int minute = cursor.getInt(cursor.getColumnIndex(COLUMN_MINUTE));
+                boolean isRecurring = cursor.getInt(cursor.getColumnIndex(COLUMN_IS_RECURRING)) == 1; // 1 is true, 0 is false
+                boolean isOn = cursor.getInt(cursor.getColumnIndex(COLUMN_IS_ON)) == 1;
+                boolean monday = cursor.getInt(cursor.getColumnIndex(COLUMN_MONDAY)) == 1;
+
+                boolean tuesday = cursor.getInt(cursor.getColumnIndex(COLUMN_TUESDAY)) == 1;
+                boolean wednesday = cursor.getInt(cursor.getColumnIndex(COLUMN_WEDNESDAY)) == 1;
+                boolean thursday = cursor.getInt(cursor.getColumnIndex(COLUMN_THURSDAY)) == 1;
+                boolean friday = cursor.getInt(cursor.getColumnIndex(COLUMN_FRIDAY)) == 1;
+                boolean saturday = cursor.getInt(cursor.getColumnIndex(COLUMN_SATURDAY)) == 1;
+
+                boolean sunday = cursor.getInt(cursor.getColumnIndex(COLUMN_SUNDAY)) == 1;
+
+                String title = cursor.getString(cursor.getColumnIndex(COLUMN_TITLE));
+                String challengeType = cursor.getString(cursor.getColumnIndex(COLUMN_CHALLENGE_TYPE));
+
+                // Assuming you have a constructor in your Alarm class that matches this signature
+                Alarm alarm = new Alarm(alarmId, hour, minute, title, challengeType, isOn, isRecurring,
+                        monday, tuesday, wednesday, thursday, friday, saturday, sunday);
+
+                alarms.add(alarm);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return alarms;
+    }
 }
